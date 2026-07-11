19e064e1be53c21dea785b66d3e150c894269507
Rajeev Singh <rajeevsingh@example.com>
2026-07-11 14:29:18 +0530

    Add Week 2 demo video

--- test_model.py
+++
@@ -1,9 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-print("Import successful")
-
-print("Loading model...")
-
-model = SentenceTransformer("all-MiniLM-L6-v2")
-
-print("Model loaded successfully!")
